9062a88e07a90e6f0529b077eb8b6a49af6509b0
Jorge Gonzalez <jdgonzalez31@gmail.com>
2022-06-17 01:02:04 -0300

    changes-header-nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,18 @@ import {Nav, NavLink } from 'react-bootstrap';
 
 import { Link } from 'react-router-dom';
 //Importacion de dependencias fontAwesome
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faHouse, faGamepad} from '@fortawesome/free-solid-svg-icons'
 import { faUserAlt} from '@fortawesome/free-solid-svg-icons'
+
+
+
 const Footer = () => {
   return (
     <>
-    <div className='main-footer '>
+    <footer>
+    <div className='main-footer'>
     <div className='container ' >
     <div className='row d-flex align-items-between'  >
         <div className='col-md-4 col-sm-4 '>
@@ -24,25 +27,30 @@ const Footer = () => {
             <h2 className='footerColumns'>Navegación</h2>
             <nav>
             <ul className='list-unstyled lists-styles' >
-                <li><FontAwesomeIcon icon={faUserAlt}/><Link to='/about-us'>About us</Link></li>
-                <li><FontAwesomeIcon icon={faGamepad}/><Link to='/products'>Productos</Link></li>
-                <li><FontAwesomeIcon icon={faHouse}/><Link to='/'>Home</Link></li>
+                <li><FontAwesomeIcon icon={faUserAlt} size="1x"/><Link to='/about-us'>About us</Link></li>
+                <li><FontAwesomeIcon icon={faGamepad}size="1x"/><Link to='/products'>Productos</Link></li>
+                <li><FontAwesomeIcon icon={faHouse} size="1x"/><Link to='/'>Home</Link></li>
             </ul>
             </nav>
         </div>
         <div className='col-md-4 col-sm-4'>
-            <h2 className='footerColumns'>Redes Sociales</h2>
-            <nav>
-            <ul className='list-unstyled lists-styles'>
-                <li><FontAwesomeIcon icon={faFacebook}/><Link to='https://www.facebook.com/'>Facebook</Link></li>
-                <li><FontAwesomeIcon icon={faInstagram}/><Link to='https://twitter.com/'>Twitter</Link></li>
-                <li><FontAwesomeIcon icon={faTwitter}/><Link to='https://www.instagram.com/'>Instagram</Link></li>
-            </ul>
-            </nav>
+            <h2 className='footerColumnsThird'>Redes Sociales</h2>
+            <div className='row '>
+                 <div className='col-md-4  list-unstyled lists-styles'>
+                    <li><Link to='https://www.facebook.com/'><FontAwesomeIcon icon={faFacebook} size="lg"/></Link></li>
+                 </div>
+                 <div className='col-md-4  list-unstyled lists-styles'>
+                    <li><Link to='https://www.facebook.com/'><FontAwesomeIcon icon={faInstagram} size="lg"/></Link></li>
+                 </div>
+                 <div className='col-md-4  list-unstyled lists-styles'>
+                    <li><Link to='https://www.facebook.com/'><FontAwesomeIcon icon={faTwitter} size="lg"/></Link></li>
+                 </div>
+            </div>
         </div>
     </div>
     </div>
     </div>
+    </footer>
     </>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,48 @@
 import React from 'react'
-
+import './Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping} from '@fortawesome/free-solid-svg-icons'
+//importaciones de componentes de bootstrap
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 const Header = () => {
   return (
     <>
-    <h1>Header component</h1>    
+      <header className='header-container'>
+      <div className='row'>
+        <div className='col-sm-4 col-md-4 col-xl-4'>
+          <h3>LOGO </h3>
+        </div>
+        <div className='col-sm-4 col-md-4 col-xl-4 pt-1'>
+            <Form className="d-flex ">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button className='button-styles'>Search</Button>
+          </Form>
+        </div>
+        <div className='col-sm-4 col-md-4 col-xl-4'>
+          <div className='row last-row-container'>
+            <div className='col-2 pt-2'>
+              <button className='btn-warning'>Logueate</button>
+            </div>
+            <div className='col-2 pt-2'>
+              <button className='btn-warning '>Entra</button>
+            </div>
+            <div className='col-4 pt-2'>
+              <FontAwesomeIcon icon={faCartShopping} size='2x'/>
+            </div>
+            <div className='col-1'>
+              <h4>Dropdown </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </header>
     </>
   )
 }
