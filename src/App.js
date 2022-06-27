7e06e26df1ef729ba06a51f6bb6f02418a58cc28
Jorge Gonzalez <jdgonzalez31@gmail.com>
2022-06-27 14:20:53 -0300

    loading spinner working

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import './Apps.css';
 //PAGES
 
 //COMPONENTS
-import Layout from "./components/layout/Layout";
+// import Layout from "./components/layout/Layout";
 // import { Form } from 'react-bootstrap';
 // import Footer from './components/Footer';
+import LoadingSpinner from './components/LoadingSpinner';
 
 function App() {
   return (
     <>
-    <Layout>
+    {/* <Layout> */}
+      <LoadingSpinner/>
       <main>
     <h1>SOY EL MAIN QUE VA A TODAS PARTES</h1>
+    <button typeof='submit'>Mostrar loading spinner</button>
       </main>
-    </Layout>
+    {/* </Layout> */}
     </>
     
   );
