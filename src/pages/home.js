11f6333388aeec7e25f6bed835ef5ae681cfc626
Jorge Gonzalez <jdgonzalez31@gmail.com>
2023-01-02 20:22:00 -0300

    first commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,13 +3,14 @@ import Hero from '../components/hero';
 import Category from '../components/category';
 import Benefits from '../components/Benefits';
 import MenuCategory from '../components/MenuCategory';
-import LoadingSpinner from '../components/LoadingSpinner';
-
+// import LoadingSpinner from '../components/LoadingSpinner';
+// import SearchNavbar from '../pages/SearchNavbar'
 
 
 const Home = () =>{
     return(
         <>
+            {/* <SearchNavbar/> */}
             <Hero/>
             <Category/>
             <Benefits/>
